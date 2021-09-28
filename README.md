a5a6d53c8bcc8baad958c5fd5c0aaf96f656b585
lichuang <whlichuang@126.com>
2021-09-28 14:43:14 +0800

    部署脚本增加一些注释

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lcmall
-以有赞为目标，开发一个构建电商小程序的云平台，希望借此广交好友
-(注：项目结构较多参考了https://github.com/linlinjava/litemall项目，虽做的不是一类东西，但依然感谢)
+- 以有赞为目标，开发一个构建电商小程序的云平台，希望借此广交好友
+(注：项目结构较多参考了 https://github.com/linlinjava/litemall 项目，虽做的不是一类东西，但依然感谢)
+- 部署方式请见deploy/bin/deploy.sh脚本
+- 部署后访问 http://localhost:8080
--- deploy/bin/deploy.sh
+++ deploy/bin/deploy.sh
@@ -3,33 +3,42 @@
 SCRIPT_PATH="$( cd "$( dirname "$0"  )" && pwd  )"
 BASE_DIR=${SCRIPT_PATH}/../../
 
+# 编译管理后台的前端模块
 cd ${BASE_DIR}/lcmall-admin && tyarn build || exit 1
 echo "build admin success"
 
+# 拷贝前端模块产出物
 rm -rf ${BASE_DIR}/docker/admin/html/dist && cp -r ${BASE_DIR}/lcmall-admin/dist ${BASE_DIR}/docker/admin/html/dist || exit 1
 echo "finish copy admin"
 
+# 确认配置文件已自己编辑好
 [ -f ${BASE_DIR}/lcmall-api/src/main/resources/application.yml ] || exit 1
 echo "test application.yml ok"
 
+# 编译后端模块
 cd ${BASE_DIR} && mvn clean package || exit 1
 echo "mvn build success"
 
+# 拷贝后端模块产出物
 cp ${BASE_DIR}/lcmall-api/target/lcmall-api-*-exec.jar ${BASE_DIR}/docker/lcmall/lcmall.jar || exit 1
 echo "copy success"
 
+# 构造数据库初始化语句
 cat ${BASE_DIR}/lcmall-db/sql/lcmall_schema.sql > ${BASE_DIR}/docker/db/init-sql/lcmall.sql || exit 1
 cat ${BASE_DIR}/lcmall-db/sql/lcmall_table.sql >> ${BASE_DIR}/docker/db/init-sql/lcmall.sql || exit 1
 echo "gen sql success"
 
 cd ${BASE_DIR}/docker || exit 1
 
+# 停服
 docker-compose down || exit 1
 echo "docker down success"
 
+# 构建镜像
 docker-compose build || exit 1
 echo "docker build success"
 
+# 启动服务
 docker-compose up -d || exit 1
 echo "docker up success"
 
