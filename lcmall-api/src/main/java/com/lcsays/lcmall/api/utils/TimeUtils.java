a2c4bbf21eb0acbff078981ccc2c6e11cfa86a9c
whlichuang@126.com <whlichuang@126.com>
2021-11-02 17:43:30 +0800

    月份值加1

--- lcmall-api/src/main/java/com/lcsays/lcmall/api/utils/TimeUtils.java
+++ lcmall-api/src/main/java/com/lcsays/lcmall/api/utils/TimeUtils.java
@@ -44,7 +44,7 @@ public class TimeUtils {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return calendar.get(Calendar.YEAR)
-                + "-" + calendar.get(Calendar.MONTH)
+                + "-" + (calendar.get(Calendar.MONTH) + 1)
                 + "-" + calendar.get(Calendar.DAY_OF_MONTH);
     }
 
