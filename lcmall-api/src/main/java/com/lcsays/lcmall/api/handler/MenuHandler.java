894873b5b209a694eb806c114ee1f3db3ab29148
lichuang <whlichuang@126.com>
2022-02-08 21:41:21 +0800

    codemeteors

--- lcmall-api/src/main/java/com/lcsays/lcmall/api/handler/MenuHandler.java
+++ lcmall-api/src/main/java/com/lcsays/lcmall/api/handler/MenuHandler.java
@@ -48,7 +48,7 @@ public class MenuHandler extends AbstractHandler {
                         request.setMchid(wxApp.getMchId());
                         request.setDescription("this is Description");
                         request.setOutTradeNo(RequestNo.randomWithCurTime("wxpay"));
-                        request.setNotifyUrl("https://cloud.lcsays.com/api/wx/" + wxApp.getAppId() + "/pay/order_notify");
+                        request.setNotifyUrl("https://cloud.codemeteors.com/api/wx/" + wxApp.getAppId() + "/pay/order_notify");
                         WxPayUnifiedOrderV3Request.Amount amount = new WxPayUnifiedOrderV3Request.Amount();
                         amount.setTotal(610);
                         request.setAmount(amount);
